9235506d45afd4aebc9b87178e93576c9d96bbe5
Georman Calderon <georve@gmail.com>
2019-08-11 13:50:16 -0400

    se agregan parametros de produccion

---
+++ UterApplication/src/main/resources/db/migration/V1.1__Insert.sql
@@ -0,0 +1,20 @@
+--
+-- Dumping data for table `driver`
+--
+
+LOCK TABLES `driver` WRITE;
+/*!40000 ALTER TABLE `driver` DISABLE KEYS */;
+INSERT INTO `driver` VALUES (1,'Calderon','A','Georman'),(2,'Calderon','A','Andry'),(4,'Calderon','B','Dayana'),(5,'Briceno','B','Julio '),(6,'Inojosa','A','Angela'),(7,'Calderon','A','Victoria '),(8,'Salas','C','Gregorio');
+/*!40000 ALTER TABLE `driver` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `vehicle`
+--
+
+LOCK TABLES `vehicle` WRITE;
+/*!40000 ALTER TABLE `vehicle` DISABLE KEYS */;
+INSERT INTO `vehicle` VALUES (3,'Toyota','A','Machito','VF234FD'),(2,'Jeep','A','Cheroque','VS54678'),(4,'Mitsubitshi','B','Montero','RC3423YX'),(5,'Mazda','C','3','SX453GF'),(6,'Mazda','A','7','XB456YR'),(7,'Mazda','C','6','XV34576');
+/*!40000 ALTER TABLE `vehicle` ENABLE KEYS */;
+UNLOCK TABLES;
+
